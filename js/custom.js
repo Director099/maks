aaff74245d1564a38572962893d376b9870a67bb
Director099 <ferz099@mail.ru>
2018-01-22 22:49:25 +0500

    Добавлено анимация и плагин

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,29 @@
 'use strict';
+
+$(function () {
+    $.scrollUp({
+        scrollText: '',
+    });
+});
+
+var wow = new WOW({
+  mobile: false
+});
+wow.init();
+
+// Плавный скол с навигации
+
+var nav = $(".navigation__item > a");
+var down = $(".scroll-down");
+
+function scroll(item) {
+  item.click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+    return false;
+  });
+};
+
+scroll(nav);
+scroll(down);
